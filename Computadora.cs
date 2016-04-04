39bfe9afa29063a01f47cc1fe9c398ba9e671be8
SalvatierraFran <franco14_93@hotmail.com>
2016-04-03 21:08:33 -0300

    Programa finalizado.

--- Computadora.cs
+++ Computadora.cs
@@ -7,5 +7,70 @@ namespace Ejercicio18
 {
     class Computadora
     {
+        private bool _estaEncendida;
+        private Enumerados.EMarca _marca;
+        private float _peso;
+        private Enumerados.EProcesador _procesador;
+
+        public Computadora(bool Encendida, Enumerados.EMarca Marca, float Peso, Enumerados.EProcesador Procesador)
+        {
+            this._estaEncendida = Encendida;
+            this._marca = Marca;
+            this._peso = Peso;
+            this._procesador = Procesador;
+        }
+
+        public bool getEstaEncendida()
+        {
+            return this._estaEncendida;
+        }
+
+        public Enumerados.EMarca getMarca()
+        {
+            return this._marca;
+        }
+
+        public float getPeso()
+        {
+            return this._peso;
+        }
+
+        public Enumerados.EProcesador getProcesador()
+        {
+            return this._procesador;
+        }
+
+        public void setEstaEncendida(bool encender)
+        {
+            this._estaEncendida = encender;
+        }
+
+        public void setMarca(Enumerados.EMarca marca)
+        {
+            this._marca = marca;
+        }
+
+        public void setPeso(float peso)
+        {
+            this._peso = peso;
+        }
+
+        public void setProcesador(Enumerados.EProcesador procesador)
+        {
+            this._procesador = procesador;
+        }
+    }
+}
+
+namespace Enumerados
+{
+    public enum EMarca
+    {
+        Asus, Apple, Samsung, Toshiba, IBM
+    }
+
+    public enum EProcesador
+    {
+        Intel_Core_i7, ADM_Phenom_II, ADM_Athlon_II, Intel_Celeron_430, ADM_Sempron_145
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ namespace Ejercicio18
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio 18";
+
+            Computadora PC1 = new Computadora(true, Enumerados.EMarca.Apple, 125, Enumerados.EProcesador.Intel_Core_i7);
+
+            Console.Write("PC Encendida: "+ PC1.getEstaEncendida());
+            Console.Write("\nMarca: " + PC1.getMarca());
+            Console.Write("\nPeso: " + PC1.getPeso());
+            Console.Write("\nProcesador: " + PC1.getProcesador());
+
+            Console.ReadKey();
         }
     }
 }
